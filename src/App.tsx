668ccb69d6c6f9b66fadbd96e2b7f8d57b87c295
Horacio <horacio.schumann@gmail.com>
2023-02-27 13:21:59 -0300

    feat: adicionada funcionalidade de edição da tarefa e melhorados alguns estilos.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import { useState, useEffect } from "react";
-import { FaList, FaTools, FaCheck, FaTrash } from "react-icons/fa";
+import {
+  FaList,
+  FaTools,
+  FaCheck,
+  FaTrash,
+  FaPencilAlt,
+  FaTimes,
+} from "react-icons/fa";
+import { Tooltip } from "react-tooltip";
+
+import "react-tooltip/dist/react-tooltip.css";
 import "./App.css";
 
 interface Task {
@@ -12,6 +22,9 @@ function App() {
   const [doingTasks, setDoingTasks] = useState<Task[]>([]);
   const [doneTasks, setDoneTasks] = useState<Task[]>([]);
   const [contentLoaded, setContentLoaded] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingTask, setEditingTask] = useState<Task>();
+  const [editingArray, setEditingArray] = useState<Task[]>([]);
 
   const [newTaskDescription, setNewTaskDescription] = useState("");
 
@@ -74,6 +87,47 @@ function App() {
     }
   };
 
+  const handleEditTask = (taskArray: Task[], taskId: number) => {
+    setIsEditing(true);
+    const taskTemp = taskArray.find((task) => task.id == taskId);
+    if (taskTemp) {
+      setEditingTask(taskTemp);
+      setEditingArray(taskArray);
+    }
+  };
+  const handleEditChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let { id, description } = editingTask as Task;
+    description = e.target.value;
+    setEditingTask({ id, description });
+  };
+
+  const handleApplyChanges = () => {
+    if (editingTask) {
+      const newTaskArray = editingArray.map((task) =>
+        task.id == editingTask.id ? (task = editingTask) : task
+      );
+      if (todoTasks == editingArray) {
+        setTodoTasks(newTaskArray);
+        window.localStorage.setItem("todoTasks", JSON.stringify(newTaskArray));
+      }
+      if (doingTasks == editingArray) {
+        setDoingTasks(newTaskArray);
+        window.localStorage.setItem("doingTasks", JSON.stringify(newTaskArray));
+      }
+      if (doneTasks == editingArray) {
+        setDoneTasks(newTaskArray);
+        window.localStorage.setItem("doneTasks", JSON.stringify(newTaskArray));
+      }
+
+      setEditingArray([]);
+      setIsEditing(false);
+    }
+  };
+
+  const handleCancelEditTask = () => {
+    setIsEditing(false);
+  };
+
   useEffect(() => {
     const todos = JSON.parse(window.localStorage.getItem("todoTasks") || "[]");
     const doings = JSON.parse(
@@ -97,12 +151,38 @@ function App() {
 
   return (
     <div className="App">
+      {isEditing && editingTask && (
+        <div className="overlay">
+          <div className="modal">
+            <input
+              type="text"
+              onChange={(e) => handleEditChange(e)}
+              value={editingTask.description}
+            />
+
+            <div className="edit-buttons">
+              <button className="button" onClick={() => handleApplyChanges()}>
+                <FaCheck />
+              </button>
+              <button
+                className="button cancel-button"
+                onClick={handleCancelEditTask}
+              >
+                <FaTimes />
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
       <div className="container">
+        <Tooltip anchorSelect=".button" />
         <h1 className="header">
           <span>
             <FaList />
           </span>{" "}
-          <span>TO</span>-<span>DO</span> <span>List</span>
+          <span>To</span>
+          <span>Do</span> <span>LIST</span>
         </h1>
         <div className="input-container">
           <input
@@ -126,20 +206,30 @@ function App() {
                   {task.description}
                   <div className="task-controls">
                     <button
+                      onClick={() => handleEditTask(todoTasks, task.id)}
+                      className="button edit-button"
+                      data-tooltip-content="Edit!"
+                    >
+                      <FaPencilAlt />
+                    </button>
+                    <button
                       onClick={() => handleDoingTask(todoTasks, task.id)}
                       className="button doing-button"
+                      data-tooltip-content="Send this task to DOING!"
                     >
                       <FaTools />
                     </button>
                     <button
                       onClick={() => handleDoneTask(todoTasks, task.id)}
                       className="button done-button"
+                      data-tooltip-content="Make it DONE!"
                     >
                       <FaCheck />
                     </button>
                     <button
                       onClick={() => handleDeleteTask(todoTasks, task.id)}
                       className="button delete-button"
+                      data-tooltip-content="DELETE"
                     >
                       <FaTrash />
                     </button>
@@ -159,20 +249,30 @@ function App() {
                   {task.description}
                   <div className="task-controls">
                     <button
+                      onClick={() => handleEditTask(doingTasks, task.id)}
+                      className="button edit-button"
+                      data-tooltip-content="Edit!"
+                    >
+                      <FaPencilAlt />
+                    </button>
+                    <button
                       onClick={() => handleToDoTask(doingTasks, task.id)}
                       className="button todo-button"
+                      data-tooltip-content="Back to 'TO-DO'"
                     >
                       <FaList />
                     </button>
                     <button
                       onClick={() => handleDoneTask(doingTasks, task.id)}
                       className="button done-button"
+                      data-tooltip-content="Make it DONE!"
                     >
                       <FaCheck />
                     </button>
                     <button
                       onClick={() => handleDeleteTask(doingTasks, task.id)}
                       className="button delete-button"
+                      data-tooltip-content="DELETE"
                     >
                       <FaTrash />
                     </button>
@@ -193,20 +293,30 @@ function App() {
                   {task.description}
                   <div className="task-controls">
                     <button
+                      onClick={() => handleEditTask(doneTasks, task.id)}
+                      className="button edit-button"
+                      data-tooltip-content="Edit!"
+                    >
+                      <FaPencilAlt />
+                    </button>
+                    <button
                       onClick={() => handleToDoTask(doneTasks, task.id)}
                       className="button todo-button"
+                      data-tooltip-content="Back to 'TO-DO'"
                     >
                       <FaList />
                     </button>
                     <button
                       onClick={() => handleDoingTask(doneTasks, task.id)}
                       className="button doing-button"
+                      data-tooltip-content="Send this task to DOING!"
                     >
                       <FaTools />
                     </button>
                     <button
                       onClick={() => handleDeleteTask(doneTasks, task.id)}
                       className="button delete-button"
+                      data-tooltip-content="DELETE"
                     >
                       <FaTrash />
                     </button>
