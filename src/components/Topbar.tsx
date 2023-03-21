eaf88de4978b3409a27d43c7aef0c32c1c60740e
Horacio <horacio.schumann@gmail.com>
2023-03-21 14:54:25 -0300

    Criado componente Topbar

---
+++ src/components/Topbar.tsx
@@ -0,0 +1,13 @@
+import { FaList } from "react-icons/fa";
+
+export default function Topbar() {
+  return (
+    <h1>
+      <span>
+        <FaList />
+      </span>{" "}
+      <span>To</span>
+      <span>Do</span> <span>LIST</span>
+    </h1>
+  );
+}
