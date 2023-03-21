eba79483eecb482e7bf5d4bbd23d8f2356a0a9dd
Horacio <horacio.schumann@gmail.com>
2023-03-21 20:32:10 -0300

    tamanho do botão diminuído

--- src/components/buttons/TaskButton.tsx
+++ src/components/buttons/TaskButton.tsx
@@ -3,7 +3,7 @@ import IconButton from "@mui/material/IconButton";
 export default function TaskButton(props: any) {
   const { taskList, taskid, handleClick, icon } = props;
   return (
-    <IconButton onClick={() => handleClick(taskList, taskid)} size="medium">
+    <IconButton onClick={() => handleClick(taskList, taskid)} size="small">
       {icon}
     </IconButton>
   );
